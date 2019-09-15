cb8908a18b24006a33a0eefbe4e576f6c3d65f6f
Gouri <gouri.josh@gmail.com>
2019-09-14 19:05:33 -0700

    added the javascript code instaed of jquery

--- assets/scripts/gamescript.js
+++ assets/scripts/gamescript.js
@@ -1,8 +1,25 @@
-$(document).ready(function () {
-    $("#letterinput").val('').focus();
+//get the controls of all the required HTML elements 
+var letterInput = document.getElementById("letterinput");
+var totalGuessesElement = document.getElementById("totalguesses");
+var guess = document.getElementById("guess");
+var nextWord = document.getElementById("nextword");
+var restart = document.getElementById("restart");
+var guessWord = document.getElementById("guessword");
+var looseValue = document.getElementById("looseValue");
+var winValue = document.getElementById("winValue");
+var message = document.getElementById("msg");
+var imagediv = document.getElementById("imgdiv");
+var wordInfo = document.getElementById("wordinfo");
+var wrongGuesses = document.getElementById("wrongguesses");
+letterInput.focus();
+
+//create the logic inside the function
+var runScript = function () {
+    
+    //hangman object 
     var hangman = {
         words: [
-            { name: "Thor", image: "assets/images/thor.jpg", info: "God Of Thunder"},
+            { name: "Thor", image: "assets/images/thor.jpg", info: "God Of Thunder" },
             { name: "captainamerica", image: "assets/images/captainamerica.png", info: "Man of the match" },
             { name: "ironman", image: "assets/images/Iron-Man.png", info: "Man of Steel" },
             { name: "BlackPanther", image: "assets/images/blackpanther.jpg", info: "King Of Wauconda" },
@@ -11,7 +28,7 @@ $(document).ready(function () {
             { name: "SpiderMan", image: "assets/images/spiderman.JPG", info: "Friendly Neighbourhood" },
             { name: "Hulk", image: "assets/images/hulk.jpg", info: "Green Monster" },
             { name: "Wolverine", image: "assets/images/wolverine.jpg", info: "Man with Claws" },
-            {name : "DoctorStrange" , image:"assets/images/drstrange.jpg" ,info:"Mystic Art"}
+            { name: "DoctorStrange", image: "assets/images/drstrange.jpg", info: "Mystic Art" }
         ],
         randomguessedword: "",
         correctGuessedLetters: [],
@@ -21,55 +38,57 @@ $(document).ready(function () {
         filledpositions: 0,
         wins: 0,
         looses: 0,
-        nextWordIndex:0,
+        nextWordIndex: 0,
     };
 
 
     var getRandomWordFromArray = function () {
-        //can be used this to generate the index when theer are many words in the list.
+        //used to generate the index when there are many words in the list.
         // var randomindex = Math.floor(Math.random() * hangman.words.length);
         // return hangman.words[randomindex];
 
         //to avoid the repeatition, i am using a loop to make sure each word appears once
-        if(hangman.nextWordIndex<hangman.words.length)
-        {
-        return hangman.words[hangman.nextWordIndex++];        
-        }
-        else{
-            hangman.nextWordIndex=0;
-            return hangman.words[hangman.nextWordIndex];
-        }
-
-        
-    };
-    var createLetterSpans = function () {
-        $("#totalguesses").text(hangman.totalGuesses);
-        for (var j = 0; j < hangman.randomguessedword.name.length; j++) {
-            var newGuessLetterDisplay = $("<span class='placeword'>");
-            $("#guessword").append(newGuessLetterDisplay);
-        }
-    };
-
-    $("#restart").on("click", function () {
-        $("#nextword").click();
-        hangman.wins = 0;
-        hangman.looses = 0;
-        $("#winValue").text(hangman.wins);
-        $("#looseValue").text(hangman.looses);
-        $("#letterinput").prop("disabled", false);
-        $("#letterinput").val('').focus();
-    });
-
-    $("#guess").on("click", function () {
-        if (hangman.totalGuesses < 0) {
-            $("#letterinput").prop("disabled", true);
-            $("#msg").text("You have no more guesses.Try next word");
+        if (hangman.nextWordIndex < hangman.words.length) {
+            return hangman.words[hangman.nextWordIndex++];
         }
         else {
-            var guessedletter = $("#letterinput").val().toUpperCase();
+            hangman.nextWordIndex = 0;
+            return hangman.words[hangman.nextWordIndex];
+        }
+    };
+
+    //Create the spans(underscores) inside the guessword div 
+    var createLetterSpans = function () {
+        totalGuessesElement.innerText = hangman.totalGuesses;
+        for (var j = 0; j < hangman.randomguessedword.name.length; j++) {
+            var newGuessLetterDisplay = document.createElement("span");
+            newGuessLetterDisplay.setAttribute("class", "placeword");
+            guessWord.append(newGuessLetterDisplay);
+        }
+    };
+
+    //restart the game setting all elements to base value
+    restart.onclick = function () {
+        nextWord.click();
+        hangman.wins = 0;
+        hangman.looses = 0;
+        winValue.innerText = hangman.wins;
+        looseValue.innerText = hangman.looses;
+        letterInput.disabled = false;
+        letterInput.focus();
+    }
+
+    //craetae a click event for the guess button
+    guess.onclick = function () {
+        if (hangman.totalGuesses < 0) {
+            letterInput.disabled = true;
+            message.innerText = "You have no more guesses.Try next word";
+        }
+        else {
+            var guessedletter = letterInput.value.toUpperCase();
             console.log("guessed letter : " + guessedletter);
-            if (jQuery.inArray(guessedletter, hangman.wrongGuessedLetters) === -1 && jQuery.inArray(guessedletter, hangman.correctGuessedLetters) === -1 && guessedletter !== '' && guessedletter.match(/[a-zA-Z]/i)) {
-                var spanchildren = $("#guessword").children(".placeword");
+            if (hangman.wrongGuessedLetters.indexOf(guessedletter) === -1 && hangman.correctGuessedLetters.indexOf(guessedletter) === -1 && guessedletter !== '' && guessedletter.match(/[a-zA-Z]/i)) {
+                var spanchildren = guessWord.getElementsByClassName("placeword");
                 var letterfoundAtIndices = [];
                 for (var n = 0; n < hangman.randomguessedword.name.length; n++) {
                     if (hangman.randomguessedword.name[n].toUpperCase() === guessedletter) {
@@ -86,70 +105,82 @@ $(document).ready(function () {
                 else {
                     hangman.wrongGuessedLetters.push(guessedletter);
                     hangman.wrongguessedvaluesString = hangman.wrongguessedvaluesString + guessedletter + ', ';
-                    $("#wrongguesses").text(hangman.wrongguessedvaluesString);
+                    wrongGuesses.innerText = hangman.wrongguessedvaluesString;
                 }
-
+                
                 hangman.totalGuesses--;
+
+                //check if the totalguesses are completed
                 if (hangman.totalGuesses === 0) {
-                    $("#letterinput").prop("disabled", true);
-                    $("#letterinput").css("background-color","white");
-                    hangman.looses++;                    
-                    $("#looseValue").text(hangman.looses);
-                    $("#msg").css('color', 'red');
-                    $("#msg").text("Sorry!! You Lost");
-                    $("#wordinfo").text("You can't gues anymore.Please Try next word");
-                    $("#imgdiv").attr("src", "assets/images/oops.png");
+                    letterInput.disabled = true;
+                    letterInput.style.backgroundColor = "white";
+                    hangman.looses++;
+                    looseValue.innerText = hangman.looses;
+                    msg.style.color = "red";
+                    message.innerText = "Sorry!! You Lost";
+                    wordInfo.innerText = "You can't gues anymore.Please Try next word";
+                    imagediv.setAttribute("src", "assets/images/oops.png");
                 }
-                $("#totalguesses").text(hangman.totalGuesses);
+                totalGuessesElement.innerText = hangman.totalGuesses;
+
+                //check if the user guessed the word 
                 if (hangman.filledpositions === hangman.randomguessedword.name.length) {
-                    $("#letterinput").prop("disabled", true);
-                    $("#letterinput").css("background-color","white");
+                    letterInput.disabled = true;
+                    letterInput.style.backgroundColor = "white";
                     hangman.wins++;
-                    $("#msg").css('color', 'green');
-                    $("#msg").text("Congratulations!!  You Won");
-                    $("#imgdiv").attr("src", hangman.randomguessedword.image);
-                    $("#wordinfo").text(hangman.randomguessedword.name.toUpperCase() + "-" + hangman.randomguessedword.info);
-                    $("#winValue").text(hangman.wins);
+                    message.style.color = "green";
+                    message.innerText = "Congratulations!!  You Won";
+                    imagediv.setAttribute("src", hangman.randomguessedword.image);
+                    wordInfo.innerText = hangman.randomguessedword.name.toUpperCase() + "-" + hangman.randomguessedword.info;
+                    winValue.innerText = hangman.wins;
 
                 }
 
             }
 
         }
+        letterInput.value="";
+        letterInput.focus();
+    };
 
-        $("#letterinput").val('').focus();
-    });
 
-    
-    $("#nextword").on("click", function () {
-        $("#guessword").empty(); 
+    //next word button click event 
+    nextWord.onclick = function () {
+        guessWord.innerHTML = "";
         hangman.totalGuesses = 12;
-        $("#totalguesses").text(hangman.totalGuesses);
+        totalGuessesElement.innerText = hangman.totalGuesses;
         hangman.randomguessedword = getRandomWordFromArray();
         createLetterSpans();
         hangman.wrongGuessedLetters = [];
         hangman.correctGuessedLetters = [];
         hangman.wrongguessedvaluesString = "";
-        $("#wrongguesses").text(hangman.wrongguessedvaluesString);
+        document.getElementById("wrongguesses").innerText = hangman.wrongguessedvaluesString;
         hangman.filledpositions = 0;
-        $("#msg").text("");
-        $("#imgdiv").removeAttr("src");
-        $("#wordinfo").text("");
-        $("#letterinput").prop("disabled", false);               
-        $("#letterinput").css("background-color","aqua");
-        $("#letterinput").val('').focus();
-    });
+        msg.innerText = "";
+        imagediv.removeAttribute("src");
+        wordInfo = "";
+        letterInput.disabled = false;
+        letterInput.style.backgroundColor = "aqua";
+        letterInput.focus();
+    };
+
+    //start the game here
+    restart.click();
+
+};
+
+//an equivalent enter key event for guess button click
+document.onkeyup = function (event) {
+    if (event.key === "Enter") {
+        console.log(event.key);
+        guess.click();
+    }
+};
+
+//triggers the execution
+runScript();
 
 
-    $("#restart").click();
-
-    $(document).keyup(function(event) {
-        if (event.key === "Enter") {
-            console.log(event.key);
-           $("#guess").click();
-        }
-    });
 
 
-});
 
